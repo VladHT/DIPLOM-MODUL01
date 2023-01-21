341f209117a7b39dbe1dd8e7c5cc71805cfdb4ef
VladHT <vl.vlad1969@mail.ru>
2023-01-21 23:40:46 +0300

    dell

--- www/script.js
+++
@@ -1,7 +0,0 @@
-    $('.btn').click(function(){
-        $('.box').fadeToggle()
-    });
-    $('.box').after('<p>Этот пример сделан на jQuery</p>');
-    $('.btn').click(function(){
-        $('.btn').text('Сделай клик')
-    });
